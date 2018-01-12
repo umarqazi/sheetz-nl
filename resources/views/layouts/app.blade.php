71655438469f3828f21756655904eb592944a139
ahmad-techverx <ahmad.farooq@gems.techverx.com>
2018-01-12 19:29:42 +0500

    weddings page done with responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,8 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="/"><img src="images/logo.png"></a>
+                        <a class="navbar-brand hidden-xs" href="/"><img src="images/logo.png"></a>
+                        <a class="navbar-brand visible-xs" href="/"><img src="images/logo_mobile.png"></a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
--- resources/views/weddings.blade.php
+++ resources/views/weddings.blade.php
@@ -21,84 +21,156 @@
     <p>Check our locations and <a href="#" class="gold">contact us</a> for wedding advice.</p>
 </div>
 <!---->
-<div class="our_hotels_content text-center">
+<div class="wedding_locations text-center">
     <h1><span>OUR WEDDING LOCATIONS</span></h1>
     @include('includes/wellness_center')
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.
+    </p>
 </div>
-<div class="common_gallery text-center">
+<div class="common_gallery amazing_day text-center">
     <h1 class="text-uppercase">
         <p><span class="gold">#</span>AMAZING DAY</p>
-
     </h1>
     <span>from our customers and members</span>
-</div>
-<!---->
-<div class="what_client_say packages text-center">
     <div class="container">
         <div class="row">
+            <div class="amazing_day clearfix">
+                <div class="col-sm-4 col-xs-12">
+                    <div class="slider_img">
+                        <a href="#"><img src="images/img11.jpg"></a>
+                    </div>
+                    <div class="slider_txt">
+                        <h3>Official wedding venues</h3>
+                        <p>The wedding venues of Sheetz among to the most beautiful of the Netherlands. Give each other the vows in an old fortress, a romantic castle or a stylish residence. All wedding venues of Bilderberg are official "House of Commons". We take care of everything.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="slider_img">
+                        <a href="#"><img src="images/img16.jpg"></a>
+                    </div>
+                    <div class="slider_txt">
+                        <h3>Perfectly arranged</h3>
+                        <p>We think along with you! In all Sheetz wedding venues we help you to make your special day unforgettable. The wedding cake, photography or a luxury dinner: you can leave it to us.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="slider_img">
+                        <a href="#"><img src="images/img13.jpg"></a>
+                    </div>
+                    <div class="slider_txt">
+                        <h3>For every budget</h3>
+                        <p>Bilderberg has space for every budget. A small wedding or a joyful party: the possibilities are endless. We help you to make your day memorable.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!---->
+<div class="what_client_say best_days text-center">
+    <div class="container-fluid">
+        <div class="row">
             <p class="view_popular">
-                What <span>our</span>
+                The <span>best day</span>
             </p>
-            <h1>Partners say</h1>
+            <h1><em>Of their lives</em></h1>
             <div class="packages_wrapper clearfix">
-                <div class="responsive3">
-                    <div class="col-sm-4 col-xs-12">
-                        <div class="package_img">
-                            <img src="images/partner.png">
+                <div class="responsive4">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+      <img class="media-object" src="images/client1.png" alt="...">
+    </a>
                         </div>
-                        <div class="package_desc text-center">
-                            <h3>Lorem Ipsum is simply</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-xs-12">
-                        <div class="package_img">
-                            <img src="images/partner.png">
-                        </div>
-                        <div class="package_desc text-center">
-                            <h3>Lorem Ipsum is simply</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="media-body text-left">
+                            <h4 class="media-heading text-uppercase">A fantastic day from beginning to end</h4>
+                            <p><em>"Our wedding at the Sheetz was simply fantastic. After a wonderful day, we enjoyed a delicious dinner in the Kromme Dissel and ended the day in style with a fabulous party in the castle."</em></p>
+                            <h4>Tamara & Louis</h4>
                         </div>
                     </div>
-                    <div class="col-sm-4 col-xs-12">
-                        <div class="package_img">
-                            <img src="images/partner.png">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+      <img class="media-object" src="images/client1.png" alt="...">
+    </a>
                         </div>
-                        <div class="package_desc text-center">
-                            <h3>Lorem Ipsum is simply</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-xs-12">
-                        <div class="package_img">
-                            <img src="images/partner.png">
-                        </div>
-                        <div class="package_desc text-center">
-                            <h3>Lorem Ipsum is simply</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="media-body text-left">
+                            <h4 class="media-heading text-uppercase">A fantastic day from beginning to end</h4>
+                            <p><em>"Our wedding at the Sheetz was simply fantastic. After a wonderful day, we enjoyed a delicious dinner in the Kromme Dissel and ended the day in style with a fabulous party in the castle."</em></p>
+                            <h4>Tamara & Louis</h4>
                         </div>
                     </div>
-                    <div class="col-sm-4 col-xs-12">
-                        <div class="package_img">
-                            <img src="images/partner.png">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+      <img class="media-object" src="images/client1.png" alt="...">
+    </a>
                         </div>
-                        <div class="package_desc text-center">
-                            <h3>Lorem Ipsum is simply</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="media-body text-left">
+                            <h4 class="media-heading text-uppercase">A fantastic day from beginning to end</h4>
+                            <p><em>"Our wedding at the Sheetz was simply fantastic. After a wonderful day, we enjoyed a delicious dinner in the Kromme Dissel and ended the day in style with a fabulous party in the castle."</em></p>
+                            <h4>Tamara & Louis</h4>
                         </div>
                     </div>
-                    <div class="col-sm-4 col-xs-12">
-                        <div class="package_img">
-                            <img src="images/partner.png">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+      <img class="media-object" src="images/client1.png" alt="...">
+    </a>
                         </div>
-                        <div class="package_desc text-center">
-                            <h3>Lorem Ipsum is simply</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="media-body text-left">
+                            <h4 class="media-heading text-uppercase">A fantastic day from beginning to end</h4>
+                            <p><em>"Our wedding at the Sheetz was simply fantastic. After a wonderful day, we enjoyed a delicious dinner in the Kromme Dissel and ended the day in style with a fabulous party in the castle."</em></p>
+                            <h4>Tamara & Louis</h4>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+<div class="wedding_form_wrapper clearfix">
+
+    <div class="col-sm-6 hidden-xs">
+        <img src="images/img10.jpg">
+    </div>
+    <div class="wedding_form col-sm-6 col-xs-12">
+        <span>REQUEST WEDDING INFORMATION</span>
+        <h2><em>Make your dream come true</em></h2>
+        <form>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Your Name and Last Name">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="E-mail...">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Question and remarks...">
+            </div>
+            <div class="hotel_filter">
+                <ul>
+                    <div class="filter_inner clearfix">
+
+                        <li class="dropdown">
+                            <span>Desired Hotel</span>
+                            <img src="images/icon2.png">
+                            <input type="text" class="form-control datepicker" placeholder="Check In">
+                        </li>
+                        <li class="dropdown">
+                            <span>Wedding date</span>
+                            <img src="images/icon1.png">
+                            <select class="selectpicker">
+                    <option><span>aaa</span></option>
+                    <option><span>aaa</span></option>
+                </select>
+                        </li>
+                    </div>
+                </ul>
+
+
+            </div>
+            <div class="view_all hvr-sweep-to-right">SEND REQUEST</div>
+        </form>
     </div>
 </div>
 
