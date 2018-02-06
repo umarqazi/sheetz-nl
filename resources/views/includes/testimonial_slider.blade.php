18aa26d99e2c137ee7bc4d7f0e4901430f406652
ahmad-techverx <ahmad.farooq@gems.techverx.com>
2018-02-06 18:16:42 +0500

    events , conferences and site color changes i,e... #424242

---
+++ resources/views/includes/testimonial_slider.blade.php
@@ -0,0 +1,22 @@
+<div class="testimonials_slider">
+    <div class="slide_wrapper text-center">
+        <img src="images/logo2.png">
+        <h4>Tamara Vorstenbosch</h4>
+        <div class="rateit" data-rateit-mode="font" data-rateit-icon="" style="font-family:fontawesome">
+        </div>
+        <div class="slider_txt">
+            <h4>IT'S AN EXTREMELY HOSPITABLE HOTEL WITH A VIEW OVER THE BEAUTIFUL CITY netherlands</h4>
+            <em>"We highly recommend this hotel as a base for visiting Rotterdam. You are helped in a very personal and attentive way. The staff are polite, fantastic, accommodating and friendly. They offer top service at every level. The breakfast is totally fresh, they serve the best coffee and think of everything, right down to the smallest details. The executive room on the 11th floor is an eye opener." </em>
+        </div>
+    </div>
+    <div class="slide_wrapper text-center">
+        <img src="images/logo2.png">
+        <h4>Tamara Vorstenbosch</h4>
+        <div class="rateit" data-rateit-mode="font" data-rateit-icon="" style="font-family:fontawesome">
+        </div>
+        <div class="slider_txt">
+            <h4>IT'S AN EXTREMELY HOSPITABLE HOTEL WITH A VIEW OVER THE BEAUTIFUL CITY netherlands</h4>
+            <em>"We highly recommend this hotel as a base for visiting Rotterdam. You are helped in a very personal and attentive way. The staff are polite, fantastic, accommodating and friendly. They offer top service at every level. The breakfast is totally fresh, they serve the best coffee and think of everything, right down to the smallest details. The executive room on the 11th floor is an eye opener." </em>
+        </div>
+    </div>
+</div>
