3d31d976f2c40afad7803a04726f40d02392d38e
Umar Farooq <umar.farooq@gems.techverx.com>
2018-02-09 00:03:38 +0500

    Dashboard Admin Panel with all css & Js Files,Login and Signup Authentication.

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 2 | Registration Page</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="/css/admin/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/css/admin/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="/css/admin/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/css/admin/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="/css/admin/blue.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+    <div class="register-logo">
+        <a href="/admin/dashboard"><b>Admin</b>LTE</a>
+    </div>
+
+    <div class="register-box-body">
+        <p class="login-box-msg">Register a new membership</p>
+
+            <form method="POST" action="{{ route('register') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
+
+                    <input id="name" type="text" class="form-control" placeholder="Full name" name="name" value="{{ old('name') }}" required autofocus>
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+
+                @if ($errors->has('name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+
+                    <input id="email" type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}" required>
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+
+                    <input id="password" type="password" class="form-control" placeholder="Password" name="password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+
+                @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group has-feedback">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Retype Password" required>
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat">
+                                Register
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        <a href="/login" class="text-center"><b>Already A Member??</b></a>
+    </div>
+</div>
+</body>
+</html>
